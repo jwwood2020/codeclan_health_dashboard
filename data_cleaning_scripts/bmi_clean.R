b31dcc104604689013c6ab747e4e7fdc5d61a2a0
jwwood2020 <78509344+jwwood2020@users.noreply.github.com>
2021-05-16 12:49:16 +0100

    cleaning scripts for BMI, birthweight

--- data_cleaning_scripts/bmi_clean.R
+++ data_cleaning_scripts/bmi_clean.R
@@ -3,7 +3,7 @@
 
 # This script reads in the "BMI Clinical" csv file, cleans names and then
 # gets the health board name from the "id_lookups" file by joining on the 
-# feature_id and hb_code variables.
+# feature_id = hb_code variables.
 # 
 # The only manual intervention occurs where feature_id = S92000003
 # This code corresponds to the country_name = "Scotland", not a health board.
@@ -18,14 +18,14 @@ library(janitor)
 
 # Read raw data -----------------------------------------------------------
 
-bmi_clinical <- read_csv(here("data_raw/bmi_p1_clinical.csv")) %>% 
+bmi_clinical <- read_csv("data_raw/bmi_p1_clinical.csv") %>% 
   clean_names() %>% 
   mutate(year = str_sub(date_code,1,4))
 
 
 # Get Health Board names --------------------------------------------------
 
-health_board_id <- read_csv(here("data_clean/id_lookups.csv")) %>% 
+health_board_id <- read_csv("data_clean/id_lookups.csv") %>% 
   select(hb_code, hb_name) %>% 
   distinct(hb_code, .keep_all = TRUE)
 
