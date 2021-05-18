269dfd08dff629040c5c7e1140a96e4ab088255e
jonnyd360 <69044665+jonnyd360@users.noreply.github.com>
2021-05-18 14:37:59 +0100

    entering graphs

--- shinny_app/health_dasboard/health_app/global.R
+++ shinny_app/health_dasboard/health_app/global.R
@@ -8,6 +8,27 @@ library(here)
 
 # John's Data (not working need to get some more context interms of the data used)
 
+smokers <- read_csv(here("data_clean/antenatal_smoking.csv")) %>% 
+  arrange(year)
+
+low_birthweight <- read_csv(here("data_clean/low_birthweight.csv")) %>% 
+  arrange(year)
+
+low_birthweight <- low_birthweight %>% 
+  left_join(smokers,
+            by = c("feature_code", "year")) 
+
+mothers_ages <- read_csv(here("data_clean/mothers_ages.csv")) %>% 
+  arrange(year) %>% 
+  mutate(year = as.character(year))
+
+# low_birthweight_full <- low_birthweight %>%
+#   left_join(mothers_ages,
+#             by = c("year", "feature_code")) %>%
+#   rename(older_ratio = ratio)
+
+
+breastfeeding <- read_csv(here("data_clean/breastfeeding.csv")) 
 
 # Emilies Data 
 
--- shinny_app/health_dasboard/health_app/server.R
+++ shinny_app/health_dasboard/health_app/server.R
@@ -5,43 +5,111 @@ shinyServer(function(input, output) {
     #Scotland Tab data
     
         output$smokers_scotland <- renderPlot({
-        smokers %>% 
-            # Can have a dropdown selector in the dashboard to filter on feature_code
-            # so that we can show council areas
+            
+             smokers %>% 
+                filter(feature_code == "S92000003") %>% 
+                ggplot() +
+                aes(x = year,
+                    y = smoker_ratio) +
+                geom_point() +
+                scale_x_continuous(breaks = 2002:2019) +
+                scale_y_continuous(limits = c(0, NA)) +
+                geom_smooth(method=lm , color="grey", se = FALSE) +
+                labs(x = "Year",
+                     y = "Percentage of antenatal smokers",
+                     title = "Fewer new mothers in Scotland are smokers") +
+                theme_minimal()
+        
+        })
+        
+        output$birth_scotland <- renderPlot({
+            low_birthweight %>% 
+                filter(feature_code == "S92000003") %>% 
+                ggplot() +
+                aes(x = year,
+                    y = weight_ratio,) +
+                geom_point() +
+                geom_smooth(method=lm , color="grey", se = FALSE) +
+                labs(x = "Year",
+                     y = "Percentage of low-weight births",
+                     title = "Fewer newborns in Scotland are being classified as having low birthweight")
+        })
+        
+        output$smokebirth_scotland <- renderPlot({
+            low_birthweight %>% 
+                filter(feature_code == "S92000003") %>% 
+                ggplot() +
+                aes(x = year, 
+                    y = smoker_ratio,
+                    size = weight_ratio
+                ) +
+                geom_point() +
+                #geom_smooth(method=lm , color="red", se = FALSE) +
+                labs(x = "Year",
+                     y = "% smoking",
+                     size = "% of births classed as low birthweight",
+                     title = "How have smoking and low birthweights in Scotland changed over time?") 
+            
+        })
+        output$mothers_ages <- renderPlot({
+            mothers_ages%>% 
+                filter(feature_code == "S92000003") %>% 
+                ggplot() +
+                aes(x = year,
+                    y = ratio,
+                    group = age_band,
+                    colour = age_band) +
+                geom_point() +
+                geom_line() +
+                scale_x_discrete(breaks = 2002:2019) +
+                scale_y_continuous(limits = c(0, NA)) +
+                #  geom_smooth(method=lm , color="red", se = FALSE) +
+                labs(x = "Year",
+                     y = "Percentage of first-time mothers",
+                     title = "Are first-time mothers in Scotland getting older?")
+        })
+        
+        low_birthweight %>% 
             filter(feature_code == "S92000003") %>% 
             ggplot() +
-            aes(x = year,
-                y = smoker_ratio) +
+            aes(x = older_ratio,
+                y = weight_ratio,) +
             geom_point() +
-            scale_x_continuous(breaks = 2002:2019) +
-            scale_y_continuous(limits = c(0, NA)) +
-            geom_smooth(method=lm , color="grey", se = FALSE) +
-            labs(x = "Year",
-                 y = "Percentage of antenatal smokers",
-                 title = "Fewer new mothers in Scotland are smokers") +
-            theme_minimal()
-        
-        })
-    
-        output$smokers_council <- renderPlot({
-        smokers %>% 
-            # Can have a dropdown selector in the dashboard to filter on feature_code
-            # so that we can select individual council areas
-            filter(feature_code %in% council_vector) %>% 
-            ggplot() +
-            aes(x = year,
-                y = smoker_ratio) +
-            geom_point() +
-            scale_x_continuous(breaks = 2002:2019) +
-            scale_y_continuous(limits = c(0, NA)) +
-            labs(x = "Year",
-                 y = "Percentage of antenatal smokers",
-                 title = "Fewer new mothers in Scotland are smokers") +
-            theme_minimal()
-        
-        })
+            geom_smooth(method=lm , color="red") +
+            labs(x = "Percentage of first-time mothers",
+                 y = "Percentage of low-weight births",
+                 title = "Is there a relationship between low birthweight and mother's age?",
+                 subtitle = "Plot of low birthweight % vs mothers age band %, Scotland 2002 - 2019") +
+            facet_wrap(~ age_band)
         
     #Council Tab data
         
+        output$smokers_council <- renderPlot({
+            smokers %>% 
+                # Can have a dropdown selector in the dashboard to filter on feature_code
+                # so that we can select individual council areas
+                ggplot() +
+                aes(x = year,
+                    y = smoker_ratio) +
+                geom_point() +
+                scale_x_continuous(breaks = 2002:2019) +
+                scale_y_continuous(limits = c(0, NA)) +
+                labs(x = "Year",
+                     y = "Percentage of antenatal smokers",
+                     title = "Antenatal smokers") +
+                theme_minimal()
+            
+        })
+        
+        output$birthweight_council <- renderPlot({
+            low_birthweight %>% 
+                ggplot() +
+                aes(x = year,
+                    y = weight_ratio,) +
+                geom_point() +
+                labs(x = "Year",
+                     y = "Percentage of low-weight births",
+                     title = "tbc")
+        })
         
 })
