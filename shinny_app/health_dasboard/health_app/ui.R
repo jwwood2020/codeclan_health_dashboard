b19c3572069e0e188089c8d597f319859bf15d2a
jonnyd360 <69044665+jonnyd360@users.noreply.github.com>
2021-05-18 11:17:02 +0100

    shinny outline complete

--- shinny_app/health_dasboard/health_app/ui.R
+++ shinny_app/health_dasboard/health_app/ui.R
@@ -8,33 +8,51 @@ library(ggplot2)
 # Define UI for application
 shinyUI(
     dashboardPage(
-        dashboardHeader(title = tags$i("Scothland Health Dasboard")), 
+        dashboardHeader(title = tags$i("Scothland Health Dasboard")),
         
         
         # Sidebar content
         dashboardSidebar(
             sidebarMenu(id = "menu1",
-                menuItem("Home", tabName = "home", icon = icon("home"))
-                ),
+                menuItem("Home", tabName = "home", icon = icon("home"))),
             
             conditionalPanel(
                 condition = "input.menu1 =='home'",
                 
                 selectInput("location_button",
                             "Which location do you want to look at?",
-                            choices = c("Need to Add")
-                ),
+                            choices = c("Need to Add")),
+                
                 radioButtons("data_button",
                              "Which data do you want to look at",
-                             choices = c("Data", "Selection")
-                )
+                             choices = c("Data", "Selection"))
             ),
                 
             sidebarMenu(id = "menu1",    
-                menuItem("Baby Data", tabName = "baby_data", icon = icon("baby")),
-                menuItem("Mother Data", tabName = "mother_data", icon = icon("plus"))
-            )
-        ),
+                menuItem("Scotland Data", tabName = "scotland_data", icon = icon("baby"))),
+                
+                conditionalPanel(
+                    condition = "input.menu1 =='scotland_data'",
+                    
+                    selectInput("location_button",
+                                "What data do you want to look at?",
+                                choices = c("Baby", "Mother"))
+                    
+                ),
+            
+                
+            sidebarMenu(id = "menu1",    
+                menuItem("Council Data", tabName = "council_data", icon = icon("plus"))),
+                
+                conditionalPanel(
+                    condition = "input.menu1 =='council_data'",
+                    
+                    selectInput("location_button",
+                                "What data do you want to look at?",
+                                choices = c("Need Council data"))
+                )
+            
+            ), # Dashboardsidebar bracket
         
         # Body content
         dashboardBody(
@@ -59,11 +77,3 @@ shinyUI(
         )
         )
 )
-    
-# tabItem(tabName = "home",
-#         h2("Home menu content"),
-#         
-#         valueBox("Quickstat1", 24524), #Needs to be valueBoxOutput to pick up data later on
-#         valueBox("Quickstat2", 24535),
-#         valueBox("Quickstat3", 42435)
-# ),
